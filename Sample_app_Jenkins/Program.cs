6607a3806f6b09c7fb04fda309aaac739b4ae731
Deepa-Raghunandan <69631886+Deepa-Raghunandan@users.noreply.github.com>
2020-12-21 13:02:38 +0530

    Add files via upload

---
+++ Sample_app_Jenkins/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_app_Jenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing the project build");
+            Console.ReadLine();
+        }
+    }
+}
